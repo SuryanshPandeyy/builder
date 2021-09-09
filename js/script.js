7db8b41e584c49352efd5fd06deb8df3c2f88d99
Suryanshpsurya <65591693+Suryanshpsurya@users.noreply.github.com>
2021-09-09 11:01:31 +0530

    uploading builder

---
+++ js/script.js
@@ -0,0 +1,54 @@
+
+var section = document.querySelector("section");
+
+
+let inputLast = document.querySelector("input.active");
+let deviceLast = document.querySelector("div.activeDevice");
+if (inputLast.checked) {
+ section.style.width = '1250px';
+}
+
+let divs = document.querySelectorAll(".device div");
+for (const div of divs) {
+ div.addEventListener("click", (e) => {
+  
+  // Store the event target in a const
+  const et = e.target;
+
+  // select active class
+  const active = document.querySelector(".show");
+
+  /* when a button is clicked, remove the active class 
+from the button that has it */
+  if (active) {
+   active.classList.remove("show");
+  }
+
+  // Add active class to the clicked element
+  et.classList.add("show");
+
+  let input = document.querySelectorAll(".deviceCheckbox");
+
+  for (let check of input) {
+   if (check.getAttribute("data-number") === div.getAttribute("data-number")) {
+    section.style.width = div.getAttribute("data-width");
+    check.setAttribute("checked", "checked");
+   }
+   else {
+    check.removeAttribute("checked");
+   }
+  }
+ });
+}
+
+$('.bars').on('click', () => {
+ $('.mobileOptions ul').addClass('showMenu');
+ $('.blocker').addClass('showBlocker');
+ $('body').css('overflow', 'hidden');
+})
+
+$('.blocker').on('click', () => {
+ $('.mobileOptions ul').removeClass('showMenu');
+ $('.blocker').removeClass('showBlocker');
+ $('body').css('overflow', 'auto');
+})
