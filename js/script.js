44e2aca62b22e755d3740c6794e6e7ca4279e62f
Suryanshpsurya <65591693+Suryanshpsurya@users.noreply.github.com>
2021-09-10 19:56:02 +0530

    update

--- js/script.js
+++ js/script.js
@@ -4,53 +4,69 @@ var section = document.querySelector("section");
 let inputLast = document.querySelector("input.active");
 let deviceLast = document.querySelector("div.activeDevice");
 if (inputLast.checked) {
- section.style.width = '1250px';
+  section.style.width = '1250px';
 }
 
 let divs = document.querySelectorAll(".device div");
 for (const div of divs) {
- div.addEventListener("click", (e) => {
-  
-  // Store the event target in a const
-  const et = e.target;
+  div.addEventListener("click", (e) => {
 
-  // select active class
-  const active = document.querySelector(".show");
+    // Store the event target in a const
+    const et = e.target;
 
-  /* when a button is clicked, remove the active class 
-from the button that has it */
-  if (active) {
-   active.classList.remove("show");
-  }
+    // select active class
+    const active = document.querySelector(".show");
 
-  // Add active class to the clicked element
-  et.classList.add("show");
+    /* when a button is clicked, remove the active class 
+  from the button that has it */
+    if (active) {
+      active.classList.remove("show");
+    }
 
-  let input = document.querySelectorAll(".deviceCheckbox");
+    // Add active class to the clicked element
+    et.classList.add("show");
 
-  for (let check of input) {
-   if (check.getAttribute("data-number") === div.getAttribute("data-number")) {
-    
-    section.style.width = div.getAttribute("data-width");
-    check.setAttribute("checked", "checked");
-   }
-   else {
-    check.removeAttribute("checked");
-   }
-  }
- });
+    let input = document.querySelectorAll(".deviceCheckbox");
+
+    for (let check of input) {
+      if (check.getAttribute("data-number") === div.getAttribute("data-number")) {
+
+        section.style.width = div.getAttribute("data-width");
+        check.setAttribute("checked", "checked");
+      }
+      else {
+        check.removeAttribute("checked");
+      }
+    }
+  });
 }
 
 $('.bars').on('click', () => {
- $('.mobileOptions ul').addClass('showMenu');
- $('.blocker').addClass('showBlocker');
- $('body').css('overflow', 'hidden');
+  $('.mobileOptions ul').addClass('showMenu');
+  $('.blocker').addClass('showBlocker');
+  $('body').css('overflow', 'hidden');
 })
 
 $('.blocker').on('click', () => {
- $('.mobileOptions ul').removeClass('showMenu');
- $('.blocker').removeClass('showBlocker');
- $('body').css('overflow', 'auto');
+  $('.mobileOptions ul').removeClass('showMenu');
+  $('.blocker').removeClass('showBlocker');
+  $('body').css('overflow', 'auto');
+});
+
+$('.codeIcon').on('click', () => {
+  $('.code').addClass('showCode');
+  $('.blockerCode').addClass('showBlockerCode');
+  $('body').css('overflow', 'hidden');
+})
+$('.blockerCode').on('click', () => {
+  $('.code').removeClass('showCode');
+  $('.blockerCode').removeClass('showBlockerCode');
+   $('body').css('overflow', 'auto');
+});
+$('.closeCode').on('click', () => {
+  $('.code').removeClass('showCode');
+  $('.blockerCode').removeClass('showBlockerCode');
+   $('body').css('overflow', 'auto');
 });
 
 $('.compBtn').on('click', () => {
@@ -61,26 +77,43 @@ $('.close').on('click', () => {
   $('#compBox').removeClass('showComp');
 });
 
+var component = document.querySelectorAll(".component");
+var frame = document.querySelector("section");
+for (var i = 0; i < component.length; i++) {
+  component[i].setAttribute('draggable', 'true');
+  component[i].setAttribute('id', "drag"+i);
+  component[i].addEventListener("dragstart", (ev) => {
+    ev.dataTransfer.setData("component", ev.target.id); 
+  });
+}
 
-function allowDrop(ev) {
+var htmlCode = document.querySelector(".htmlCode");
+function updateCookie() {
+  localStorage.setItem("html", htmlCode.innerText);
+}
+
+
+var frameDiv = document.querySelectorAll('section div');
+
+frame.addEventListener("drop", (ev) => {
   ev.preventDefault();
-  $('#compBox').removeClass('showComp');
-}
-
-function drag(ev) {
-  ev.dataTransfer.setData("text/html", ev.target.id) ;
-}
-
-function drop(ev) {
-  ev.preventDefault(); 
-  var data = ev.dataTransfer.getData("text/html");
-  /* If you use DOM manipulation functions, their default behaviour it not to 
-  copy but to alter and move elements. By appending a ".cloneNode(true)", 
-  you will not move the original element, but create a copy. */
+  var data = ev.dataTransfer.getData("component");
   var nodeCopy = document.getElementById(data).cloneNode(true);
-  nodeCopy.id = "newComponent"; /* We cannot use the same ID */
   ev.target.appendChild(nodeCopy);
-  var div1 = document.querySelector("section");
-  console.log(div1);
-}
+  
+  for (i = 0; i < frameDiv.length; i++) {
+    frameDiv[i].setAttribute("id", "component"+i);
+  }
+  htmlCode.innerText ="<section>"+ frame.innerHTML + "</section>";
+  updateCookie();
 
+});
+
+frame.addEventListener("dragover", (ev) => {
+  ev.preventDefault();
+  
+  $('#compBox').removeClass('showComp');
+});
+
+frame.innerHTML = localStorage.getItem('html');
+htmlCode.innerText = frame.innerHTML;
